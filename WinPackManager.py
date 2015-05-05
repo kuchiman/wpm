9f35ab8e375132158deb5ff9ac29a5854e267707
kuchiman <pro@ssau.ru>
2015-05-05 18:36:24 +0400

    Попробовал нследовать классы от парсера ini файлов

--- WinPackManager.py
+++ WinPackManager.py
@@ -5,6 +5,159 @@ import subprocess
 import configparser
 
 
+class RepoCool(configparser.ConfigParser):
+    """Класс описывает репозиторий и его основные методы
+    (общие для всех репозиториев)"""
+
+    def __init__(self, name, repo_dir):
+        """Конструктор заполняет свойства класса. Как видно из кода имя файла
+        индекса захардкожено"""
+        super(RepoCool, self).__init__()
+        self.REPO_NAME = name
+        self.REPO_DIR = repo_dir
+        self.read(os.path.join('', self.REPO_DIR, 'index.ini'))
+
+    def list(self):
+        """Функция возвращает список доступных в репозитории пакетов"""
+        return [[name, self[name]['version']] for name in self.sections()]
+
+    def search(self, pkg_name):
+        """Функция ищет пакет в репозитории, если находит возвращает версию"""
+        try:
+            return self[pkg_name]['version']
+        except KeyError:
+            return None
+
+    def list_dependences(self, pkg_name):
+        """Функция возвращает список зависимостей если они есть"""
+        try:
+            return self[pkg_name]['dependences'].replace(' ', '').split(",")
+        except KeyError:
+            return []
+
+
+class LocalRepoCool(RepoCool):
+    """Частный вид репозитория, отличается от остальных тем что может
+    изменяться из программы"""
+    def __init__(self):
+        super(LocalRepoCool, self).__init__()
+        self.INDEX = os.path.join('', self.REPO_DIR, 'index.ini')
+        try:
+            self.read(self.INDEX)
+        except NameError:
+            try:
+                os.makedirs(self.REPO_DIR)  # Создана директория
+            except FileExistsError:
+                pass
+            open(self.INDEX, 'w+').close()  # Создан пустой индекс
+            self.read(self.INDEX)
+
+    def list_update(self, repo):
+        """Функция выводит список доступных для обновления пакетов"""
+        return [[name, self[name]['version'], version]
+            for name, version in repo.list()
+            if name in self
+            if self[name]['version'] < version]
+
+    def write_index(self):
+        """Запись содержимого локальной переменной в файл индекса"""
+        with open(self.INDEX, 'w') as indexfile:
+            self.write(indexfile)
+
+    def change_index(self, action, pkg_name, repo=None):
+        """Функция добавляет или удаляет запись о пакете в системной
+        переменной(не в файле индекса) Первый аргумент это необходимое действие
+        а второй имя пакета. Экземпляр класса Repo является необязательным для
+        части операций"""
+        try:
+            CACHE_PKG = self[pkg_name]
+        except KeyError:
+            self[pkg_name] = {}
+            CACHE_PKG = self[pkg_name]
+
+        if action == 'delete':               # Удаление записи о пакете
+            del CACHE_PKG
+        else:                     # Добавление записи о пакете или обновление
+            PKG = repo[pkg_name]
+            CACHE_PKG['version'] = PKG['version']
+
+            if 'file' in PKG:
+                CACHE_PKG['file'] = PKG['file']
+            elif action == 'update' and 'file' in CACHE_PKG:
+                del CACHE_PKG['file']
+
+            if 'dependences' in PKG:
+                CACHE_PKG['dependences'] = PKG['dependences']
+            elif action == 'update' and 'dependences' in CACHE_PKG:
+                del CACHE_PKG['dependences']
+
+    def pkg_download(self, pkg_name, repo):
+        """Функция загружает пакет из репозитория в кэш"""
+        pkg_version = repo[pkg_name]['version']
+        pkg_file = repo[pkg_name]['file']
+        src = os.path.join('', repo.REPO_DIR, pkg_file)
+        name_dir = os.path.join('', self.REPO_DIR, pkg_name)
+        dst = os.path.join('', name_dir, pkg_version)
+
+        try:
+            os.makedirs(name_dir)
+        except FileExistsError:
+            pass
+        try:
+            os.makedirs(dst)
+        except FileExistsError:
+            shutil.rmtree(dst)
+            os.makedirs(dst)
+
+        shutil.unpack_archive(src, dst, 'zip')
+
+    def run_script(self, soft_dir, action):
+        """Запуск скрипта с набором ключей"""
+        p = subprocess.call(['python',
+            os.path.join('', soft_dir, 'script.py'), action],
+            shell=False, stdout=subprocess.PIPE, cwd=soft_dir)
+
+    def pkg_install(self, pkg_name, repo):
+        """Функция устанавливает пакет с указанным именем."""
+        pkg_version = repo.search(pkg_name)
+        if not pkg_version:
+            return 1
+
+        soft_dir = os.path.join('', self.REPO_DIR, pkg_name, pkg_version)
+
+        cachepkg_version = self.search(pkg_name)
+        if cachepkg_version:  # Если пакет уже установлен
+            if cachepkg_version < pkg_version:
+                self.pkg_download(pkg_name, repo)
+                self.run_script(soft_dir, 'install')
+                self.change_index('update', pkg_name, repo)
+                return 3  # Обновлён
+            return 2  # Уже установлен
+        else:
+            if 'file' in repo[pkg_name]:
+                self.pkg_download(pkg_name, repo)
+                self.run_script(soft_dir, 'install')
+            self.change_index('write', pkg_name, repo)
+            return 4  # Установлен
+
+    def pkg_remove(self, pkg_name):
+        """Функция удаляет ранее установленный пакет"""
+        pkg_version = self.search(pkg_name)
+        if pkg_version:     # Проверяем есть ли такой
+            soft_dir = os.path.join('', self.REPO_DIR, pkg_name, pkg_version)
+            try:
+                self.run_script(soft_dir, 'remove')
+            except NotADirectoryError:
+                print("Кэшь повреждён!!")
+                sys.exit()
+            except FileNotFoundError:
+                print("Кэшь повреждён!!")
+                sys.exit()
+            shutil.rmtree(soft_dir)
+            self.change_index('delete', pkg_name)
+        else:
+            return 1
+
 class Repo():
     """Класс описывает репозиторий и его основные методы
     (общие для всех репозиториев)"""
@@ -13,9 +166,7 @@ class Repo():
         индекса захардкожено"""
         self.NAME = name
         self.REPO_DIR = repo_dir
-        #self.INDEX = os.path.join('', self.REPO_DIR, 'index.ini')
         self.PKGLIST = configparser.ConfigParser()
-        #self.PKGLIST.read(self.INDEX)
         self.PKGLIST.read(os.path.join('', self.REPO_DIR, 'index.ini'))
 
     def __repr__(self):
@@ -209,7 +360,7 @@ class WPM():
         try:
             for name in config['REPOSITORY']:
                 try:
-                    repos.append(Repo(name, config['REPOSITORY'][name]))
+                    repos.append(RepoCool(name, config['REPOSITORY'][name]))
                 except KeyError as e:
                     print("Отсутствует индекс репозитория " + self.NAME)
                     print(e)
@@ -220,7 +371,7 @@ class WPM():
             sys.exit()
 
         try:
-            localrepo = LocalRepo('local', config['CACHE']['dir'])
+            localrepo = LocalRepoCool('local', config['CACHE']['dir'])
         except KeyError as e:
             print("Конфигурационный файл повреждён!! Не указан адрес кэша")
             print(e)
