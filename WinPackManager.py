87a62ce6c1080d024dc9e80d9c4201c4a4410e43
kuchiman <pro@ssau.ru>
2015-05-05 14:40:15 +0400

    d

--- WinPackManager.py
+++ WinPackManager.py
@@ -281,7 +281,7 @@ class WPM():
             pkgs.pop(0)
             try:
                 repo = self.check_pkg(pkg)
-                print(type(dep))
+                print(type(repo))
                 return repo.list_dependences(pkg).extend(self.resolv_level_dependences(pkgs))
             except PackNameErr as e:
                 print("Пакет с таким именем не существует ")
